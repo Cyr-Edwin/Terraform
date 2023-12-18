ba5812b85d9e3796359a9e837020e22a9fafa72d
pyEdwin <cyredwin1@gmail.com>
2023-12-18 12:35:20 -0500

    add Exercice_3

---
+++ Terraform/Exercice_3/main.tf
@@ -0,0 +1,112 @@
+        provider "aws" {
+     region     = var.region
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+resource "aws_s3_bucket" "s3" {
+    bucket = var.bucket_name
+}
+
+resource "aws_s3_object" "object" {
+
+    for_each = fileset("html/", "*")
+    bucket = aws_s3_bucket.s3.id
+    key = each.value
+    source = "html/${each.value}"
+    etag = filemd5("html/${each.value}")
+    content_type = "text/html"
+}			
+
+resource "aws_instance" "ec2" {
+    ami = "YOUR_AMI"
+    instance_type = "t2.micro"
+    subnet_id = "${aws_default_subnet.default_az1.id}"
+    security_groups = ["${aws_security_group.web-sg.id}"]
+    iam_instance_profile = "${aws_iam_instance_profile.SSMRoleForEC2.name}"
+    user_data = <<EOF
+
+    #!/bin/bash
+    sudo su
+    yum update -y
+    yum install httpd -y
+    aws s3 cp s3://${aws_s3_bucket.s3.id}/index.html  /var/www/html/index.html
+    systemctl start httpd
+    systemctl enable httpd
+    EOF
+
+  tags = {
+    Name = "Whiz-EC2-Instance"
+  }
+}			
+
+resource "aws_security_group" "web-sg" {
+  name = "Web-SG"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}			
+
+
+resource "aws_iam_role" "role" {
+    name = "SSMRoleForEC2"
+    assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "SSMRoleForEC2" {
+    name = "SSMRoleForEC2"
+    role = "${aws_iam_role.role.name}"
+}			
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-1a"
+  #vpc_id = "${aws_default_vpc.default.id}"
+
+  tags = {
+    Name = "Default subnet for us-west-2a"
+  }
+}
+resource "aws_iam_role_policy_attachment" "role-policy-attachment" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore", 
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  ])
+
+  role       = "${aws_iam_role.role.name}"
+  policy_arn = "${each.value}"
+}			
---
+++ Terraform/Exercice_3/variable.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "value"
+}
+
+
+variable "access_key" {
+  description = "value"
+  
+}
+
+variable "secret_key" {
+  description = "value"
+  
+}
+variable "bucket_name" {
+
+}
